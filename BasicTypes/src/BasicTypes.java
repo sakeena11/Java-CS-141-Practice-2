5773a211c8266e26c583558992c9985098f95ede
sakeena11 <88400044+sakeena11@users.noreply.github.com>
2021-09-02 22:42:07 -0500

    Added feature to ask  for user's name and personalize responses

--- BasicTypes/src/BasicTypes.java
+++ BasicTypes/src/BasicTypes.java
@@ -12,30 +12,37 @@ public class BasicTypes {
     public static void main(String[] args) {
         Scanner keyScan = new Scanner(System.in);
 
+        System.out.println("Hello, what is your name?");
+        String name = keyScan.nextLine(); //set
+
         //ask the user for their year of birth and then calculate their age this year, 2021
-        System.out.println("Hello. What year were you born?");
+        System.out.println("What year were you born?");
         int yearOfBirth = keyScan.nextInt();  //reads in an integer from the keyboard and stores it in the variable
         int age = 2021 - yearOfBirth; //calculate the age of the user by subtracting year of birth from current age
-        System.out.println("Ok, this year you are " + age + " years old.");
+        System.out.println("Ok," + name + " this year you are " + age + " years old.");
+
 
         //ask the user for their height (in feet and inches)
         //then calculate and print their height in just inches and in meters
-        System.out.println("How tall are you? Please enter in feet and inches.");
+        System.out.println("How tall are you " + name + " ? Please enter in feet and inches.");
         System.out.println("Feet:");
         int heightFeet = keyScan.nextInt(); //read in the user's height (the feet part of it)
+
         System.out.println("Inches:");
         int heightInches = keyScan.nextInt(); //get user's height (the inches part of it)
         int heightTotalInches = heightFeet * 12 + heightInches;  //note 12 inches per foot
-        System.out.println("You are " +heightTotalInches + " inches tall.");
+        System.out.println(name + ", you are " +heightTotalInches + " inches tall.");
         double heightMeters = heightTotalInches * 0.0254; //note: 0.0254 meters per inch
         System.out.println("You are " + heightMeters + " meters tall.");
 
-        System.out.println("Thanks for all that personal info! You deserve some free donuts!");
+
+        System.out.println("Wow! Thanks for all that personal info " + name + "! You deserve some free donuts!");
         System.out.println("How many free donuts would you like?");
         int numOfDonuts = keyScan.nextInt();  //get the number of requested donuts
         //we hold 12 donuts in each box, extras go in a bag
         int numOfBoxes = numOfDonuts / 12; //integer division will give us the number of dozens
         int leftOverDonuts = numOfDonuts % 12; //extra donuts that don't make a dozen
+
         System.out.println("Ok, here are " + numOfBoxes + " dozen donuts and a bag with " +
                 leftOverDonuts + " extra donuts");
     }
